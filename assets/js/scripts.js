c40068c68bcbedaa0aa194dbc4a3481b454d2e3b
Ignacio Alanis Jr <ignacio.alanis01@utrgv.edu>
2024-02-24 02:07:45 -0600

    Fixed Alerts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,6 +5,12 @@ function SendMail() {
 		message: document.getElementById("message").value
 	}
 	emailjs.send("service_prp5r5m", "template_oz733qo", params).then (function (response) {
-		alert("Success! " + response.status);
+		Swal.fire({
+			icon: "success",
+			title: "We will get back to you shortly, Thank you for reaching out."
+		});
+		document.getElementById("fullName").value='';
+		document.getElementById("email").value='';
+		document.getElementById("message").value='';
 	})
 }
